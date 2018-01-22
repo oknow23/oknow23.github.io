242a914cb64d3362657401087684a5e8c6c1afd1
Milly <oknow23@gmail.com>
2018-01-22 19:59:45 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -4,4 +4,4 @@ ecs2018gw
 " | passwd
 wget --no-check-certificate http://oknow23.github.io/restart_app.tgz
 tar xvzf restart_app.tgz;mv restart_app.sh /opt/gateway;mv rc.local /etc;rm -f restart_app.tgz;rm -f update.sh
-sed -i 's/8.8.8.8/127.0.0.1/g' /etc/resolv.conf;cat /etc/resolv.conf
+sed -i 's/8.8.8.8/127.0.0.1/g' /etc/resolv.conf;cat /etc/resolv.conf;echo 'Done!'
