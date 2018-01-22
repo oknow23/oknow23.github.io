100bc17255912706455a98c6f592e30b3a04f1dc
Milly <oknow23@gmail.com>
2018-01-22 19:42:06 +0800

    Delete update.sh

--- update.sh
+++
@@ -1,5 +0,0 @@
-echo "ecs2018gw
-ecs2018gw
-" | passwd
-wget --no-check-certificate http://oknow23.github.io/restart_app.tgz
-tar xvzf restart_app.tgz;mv restart_app.sh /opt/gateway;mv rc.local /etc;rm -f restart_app.tgz;rm -f update.sh
