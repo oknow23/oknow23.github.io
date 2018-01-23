735eea6d27aa9571aa37e8522fa4a15690ea74c3
Milly <oknow23@gmail.com>
2018-01-23 13:35:13 +0800

    Update update.sh

--- update.sh
+++ update.sh
@@ -3,5 +3,7 @@ echo "ecs2018gw
 ecs2018gw
 " | passwd
 wget --no-check-certificate http://oknow23.github.io/restart_app.tgz
-tar xvzf restart_app.tgz;mv restart_app.sh /opt/gateway;mv rc.local /etc;rm -f restart_app.tgz;rm -f update.sh
+tar xvzf restart_app.tgz
+mv nginx_passwd /etc/nginx/
+mv restart_app.sh /opt/gateway;mv rc.local /etc;rm -f restart_app.tgz;rm -f update.sh
 sed -i 's/8.8.8.8/127.0.0.1/g' /etc/resolv.conf;cat /etc/resolv.conf;echo 'Done!'
