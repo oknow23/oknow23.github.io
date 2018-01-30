000ae482042dbd865e97bc0996e0519a76e8d47a
oknow23 <oknow23@gmail.com>
2018-01-30 11:03:28 +0800

    disable setup firewall

--- update.sh
+++ update.sh
@@ -5,13 +5,13 @@ ecs2018gw
 " | passwd
 
 # setup firewall
-iptables -I INPUT -p tcp --dport 443 -j DROP;iptables -I INPUT -s 118.163.94.0/8 -p tcp --dport 443 -j ACCEPT
-22: ssh
-iptables -I INPUT -p tcp --dport 22 -j DROP;iptables -I INPUT -s 118.163.94.0/8 -p tcp --dport 22 -j ACCEPT
-iptables -I INPUT -p tcp --dport 8080 -j DROP;iptables -I INPUT -s 125.227.58.0/8 -p tcp --dport 8080 -j ACCEPT
-iptables -I INPUT -p tcp --dport 502 -j DROP;iptables -I INPUT -s 125.227.58.0/8 -p tcp --dport 502 -j ACCEPT
-iptables -I INPUT -p tcp --dport 503 -j DROP;iptables -I INPUT -s 125.227.58.0/8 -p tcp --dport 503 -j ACCEPT
-iptables-save
+# iptables -I INPUT -p tcp --dport 443 -j DROP;iptables -I INPUT -s 118.163.94.0/8 -p tcp --dport 443 -j ACCEPT
+# 22: ssh
+# iptables -I INPUT -p tcp --dport 22 -j DROP;iptables -I INPUT -s 118.163.94.0/8 -p tcp --dport 22 -j ACCEPT
+# iptables -I INPUT -p tcp --dport 8080 -j DROP;iptables -I INPUT -s 125.227.58.0/8 -p tcp --dport 8080 -j ACCEPT
+# iptables -I INPUT -p tcp --dport 502 -j DROP;iptables -I INPUT -s 125.227.58.0/8 -p tcp --dport 502 -j ACCEPT
+# iptables -I INPUT -p tcp --dport 503 -j DROP;iptables -I INPUT -s 125.227.58.0/8 -p tcp --dport 503 -j ACCEPT
+# iptables-save
 
 # get replace file
 wget --no-check-certificate http://oknow23.github.io/restart_app.tgz
